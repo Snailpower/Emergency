80850ea8667b1ed553b9145be2d2eb9f5526e8a4
idea-PC\Olivier <olivier_tijhuis@hotmail.com>
2016-12-05 12:10:53 +0100

    Plus diggin (test)

--- 2eBlokProject2016/Assets/Scripts/RaycastScript.cs
+++ 2eBlokProject2016/Assets/Scripts/RaycastScript.cs
@@ -8,6 +8,7 @@ public class RaycastScript : MonoBehaviour {
     private GameObject PickedObject;
 
     private Vector2 ObjectPoint;
+    private Vector2 digginObjectPoint;
 
     private float distanceFromPlayer = 2.0f;
     private float throwForce = 2000.0f;
@@ -15,6 +16,8 @@ public class RaycastScript : MonoBehaviour {
     private Rigidbody2D rigidbodyPickedObject;
 
     private KeyCode PickAndThrowInput;
+
+ 
     
     void Start()
     {
@@ -32,11 +35,14 @@ public class RaycastScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         ObjectPoint = new Vector2(transform.position.x, transform.position.y + distanceFromPlayer);
+        digginObjectPoint = new Vector2(transform.position.x, transform.position.y + distanceFromPlayer + 5);
 
         if (pickedUp == true)
         {
             PickedObject.transform.position = ObjectPoint;
         }
+
+        Digging();
     }
 
     void FixedUpdate() {
@@ -88,5 +94,98 @@ public class RaycastScript : MonoBehaviour {
 
         rigidbodyPickedObject.AddForce(transform.right * throwForce);
         pickedUp = false;
+    }
+
+    void Digging()
+    {
+        if(Input.GetKey(KeyCode.DownArrow) && Input.GetKey(KeyCode.Space))
+        {
+            DoDigging();
+        }
+
+        if(Input.GetKey(KeyCode.LeftArrow) && Input.GetKey(KeyCode.Space))
+        {
+            DoDiggingLeft();
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow) && Input.GetKey(KeyCode.Space))
+        {
+            DoDiggingRight();
+        }
+    }
+
+    void DoDiggingLeft()
+    {
+        // cast one raycast to the left
+        Vector2 raycastToLeft = new Vector2(transform.position.x - 0.2f, transform.position.y);
+        RaycastHit2D hit = Physics2D.Raycast(raycastToLeft, Vector2.left, 10);
+
+        Debug.DrawRay(raycastToLeft, Vector2.left, Color.green, 5);
+
+        if (hit.collider != null)
+        {
+            if(hit.collider.gameObject.tag == "Ground")
+            {
+                // change position
+                GameObject other = hit.collider.gameObject;
+                other.transform.position = digginObjectPoint;
+                other.GetComponent<Rigidbody2D>().AddForce(Vector2.up * throwForce);
+            }
+        }
+    }
+
+    void DoDiggingRight()
+    {
+        //  cast one raycast to the right
+        Vector2 raycastToRight = new Vector2(transform.position.x + 0.2f, transform.position.y);
+        RaycastHit2D hit = Physics2D.Raycast(raycastToRight, Vector2.right, 10);
+
+        Debug.DrawRay(raycastToRight, Vector2.right, Color.green, 5);
+
+        if (hit.collider != null)
+        {
+            if (hit.collider.gameObject.tag == "Ground")
+            {
+                GameObject other = hit.collider.gameObject;
+                other.transform.position = digginObjectPoint;
+                other.GetComponent<Rigidbody2D>().AddForce(Vector2.up * throwForce);
+            }
+        }
+    }
+
+    void DoDigging()
+    {
+        // shoots two raycast downwards and the object it hits will be positioned above the player and shoot upwards
+        Vector2 raycastPositionRight = new Vector2(transform.position.x, transform.position.y - 0.5f);
+        Vector2 raycastPositionLeft = new Vector2(transform.position.x - 0.5f, transform.position.y - 0.5f);
+
+        RaycastHit2D hitRight = Physics2D.Raycast(raycastPositionRight, Vector2.down, 40);
+        RaycastHit2D hitLeft = Physics2D.Raycast(raycastPositionLeft, Vector2.down, 40);
+
+        Debug.DrawRay(raycastPositionRight, Vector2.down, Color.yellow, 5);
+        Debug.DrawRay(raycastPositionLeft, Vector2.down, Color.yellow, 5);
+
+        // Right raycast
+        if (hitRight.collider != null)
+        {
+            if (hitRight.collider.gameObject.tag == "Ground")    //  it works but it hits the player first
+            {
+                GameObject other = hitRight.collider.gameObject;
+
+                other.transform.position = ObjectPoint; // change position
+                other.GetComponent<Rigidbody2D>().AddForce(Vector2.up * throwForce);
+            }
+        }
+        //  Left raycast
+        if (hitLeft.collider != null)
+        {
+            if (hitLeft.collider.gameObject.tag == "Ground")    //  it works but it hits the player first
+            {
+                GameObject other = hitRight.collider.gameObject;
+
+                other.transform.position = ObjectPoint; // change position
+                other.GetComponent<Rigidbody2D>().AddForce(Vector2.up * throwForce);
+            }
+        }
     }
 }

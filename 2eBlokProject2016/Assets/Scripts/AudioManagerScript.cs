72e0fadba1cecdae40d843386e6637a1fd6e7f5c
idea-PC\Olivier <olivier_tijhuis@hotmail.com>
2017-02-01 00:54:02 +0100

    added visuals and audio

---
+++ 2eBlokProject2016/Assets/Scripts/AudioManagerScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManagerScript : MonoBehaviour {
+
+    //  explosion sounds
+    public AudioClip medium_explosionSoundsClip;
+
+    //  sound when object is being thrown
+    public AudioClip throuwSoundClip;
+
+    //  sound when player is being damaged
+    public AudioClip damageSoundClip;
+
+    //  sound when player place the object
+    public AudioClip objectPlacedSoundClip;
+
+    //  sound when objects collide
+    public AudioClip collisionSoundClip;
+
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private float RandomNumber(float min, float max)
+    {
+        float randomNumber = Random.Range(min, max);
+        return randomNumber;
+    }
+
+    public void PlayExplosionMedium(){
+        audioSource.PlayOneShot(medium_explosionSoundsClip, 0.6f);
+    }
+
+    public void PlayThrowSound(){
+        audioSource.PlayOneShot(throuwSoundClip, 1 );
+    }
+
+    public void PlayDamageSound() {
+        audioSource.PlayOneShot(damageSoundClip, 0.6f);
+    }
+
+    public void PlayObjectPlacementSound(){
+        audioSource.PlayOneShot(objectPlacedSoundClip, 0.6f);
+    }
+
+    public void PlayCollisionSound(){
+        audioSource.PlayOneShot(collisionSoundClip, 0.6f);
+    }
+}
---
+++ 2eBlokProject2016/Assets/Scripts/ParticleManagerScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleManagerScript : MonoBehaviour
+{
+
+    public GameObject smallSparkPrefab;
+    public GameObject bigSparkPrefab;
+    public GameObject afterBurnerPrefab;
+    public GameObject explosionPrefab;
+    public GameObject damageSoundPrefab;
+    
+    //  for throwing
+    public void SpawnSmallSpark(Vector2 position)
+    {
+        GameObject smallSpark = Instantiate(smallSparkPrefab, position, Quaternion.identity);
+        Destroy(smallSpark, 1);
+    }
+
+    // for collision
+    public void SpawnBigSpark(Vector2 position)
+    {
+        GameObject bigSpark = (GameObject)Instantiate(bigSparkPrefab, position, Quaternion.identity);
+        Destroy(bigSpark, 1);
+    }
+    //  for placement
+    public void SpawnAfterBurner(Vector2 position)
+    {
+        GameObject afterBurner = Instantiate(afterBurnerPrefab, position, Quaternion.identity);
+        Destroy(afterBurner, 0.2f);
+    }
+    //  for explosion
+    public void SpawnExplosion(Vector2 position)
+    {
+        GameObject explosion = Instantiate(explosionPrefab, position, Quaternion.identity);
+        Destroy(explosion, 2f);
+    }
+
+    public void SpawnDamageSoundPlayer(Vector2 position)
+    {
+        GameObject soundObject = Instantiate(damageSoundPrefab, position, Quaternion.identity);
+        Destroy(soundObject, 1);
+    }
+}
